6fb0d403acb313881011e47862611606ea659e90
Marcel Gattinger <marcel.gattinger@tum.de>
2020-06-14 01:46:16 +0200

    Basic context added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function Parent ({name}) {
+const Context = React.createContext();
+
+function Parent () {
 	return (
 		<div>
 			<h1>Parent</h1>
-			<Child name={name}/>
+			<Child/>
 		</div>
 	)
 }
 
-function Child ({name}) {
+function Child () {
 	return (
 		<div>
 			<h1>Child</h1>
-			<Grandchild name={name}/>
+			<Grandchild/>
 		</div>
 	)
 }
 
 function Grandchild ({name}) {
 	return (
-		<div>
-			<h1>Grandchild</h1>
-			<h3>{name}</h3>
-		</div>
+			<Context.Consumer>
+				{(name) => (
+					<div>
+						<h1>Grandchild</h1>
+						<h3>{name}</h3>
+					</div>
+					)
+				}
+			</Context.Consumer>
 	)
 }
 
@@ -33,7 +40,9 @@ class App extends React.Component {
 		const name = "Marcel"
 
 		return (
-			<Parent name={name}/>
+			<Context.Provider value={name}>
+				<Parent name={name}/>
+			</Context.Provider>
 		)
 	}
 }
